27dfb1b07a05e5aee40a9d4fbaa8dfb4c68e930f
Darren Atienza <darrenatienza@gmail.com>
2020-12-27 00:48:37 +0800

    update config for cors

--- config.php
+++ config.php
@@ -5,27 +5,13 @@ $setting = [
     'port' => '3306',
     'username' => 'root',
     'password' => '',
-    'database' => 'relief',
+    'database' => 'st_mesa_db',
     // 'debug' => false,
     // middlewares 
-    'middlewares' => 'dbAuth,authorization,sanitation,validation',
+    'middlewares' => 'dbAuth,authorization,sanitation,validation,cors',
     'dbAuth.mode' => 'optional',//'required'
     'dbAuth.registerUser'=> '1', // enable user registration
     'dbAuth.passwordLength' => '0',
-    'authorization.tableHandler' => function ($operation, $tableName) {
-        $valid = true;
-        if(!array_key_exists('user',$_SESSION) && $operation === 'create' && $tableName === 'students'){
-            $valid = true;
-        }
-        if(!array_key_exists('user',$_SESSION) && $operation === 'create' && $tableName === 'volunteers'){
-            $valid = true;
-        }
-        if(array_key_exists('user',$_SESSION) && $tableName != 'users'){
-            
-            $valid = true;
-        }   
-        return true;
-    },
     'sanitation.handler' => function ($operation, $tableName, $column, $value) {
         if($tableName == 'users' && $column['name'] == 'password')
             return(password_hash($value,PASSWORD_DEFAULT));
