cc12aa883f4dc85d49db442f7f4d9f67c2cb080c
Darren Atienza <darrenatienza@gmail.com>
2021-01-12 16:59:54 +0800

    update config

--- config.php
+++ config.php
@@ -15,7 +15,7 @@ $setting = [
     'dbAuth.registerUser'=> 1, // enable user registration
     'dbAuth.passwordLength' => 0,
     'sanitation.handler' => function ($operation, $tableName, $column, $value) {
-        if($tableName == 'users' && $column['name'] == 'password')
+        if($tableName == 'users' && $column['username'] == 'password')
             return(password_hash($value,PASSWORD_DEFAULT));
         return is_string($value) ? strip_tags($value) : $value;
     },
